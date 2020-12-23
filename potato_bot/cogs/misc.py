9f2d4a57d239c63080db7b49ee23969ac29d6b44
Eugene <gish.ee18@gmail.com>
2020-12-24 00:02:14 +0300

    Sort ban list by usernames, better formatting, render duration

--- potato_bot/cogs/misc.py
+++ potato_bot/cogs/misc.py
@@ -3,7 +3,7 @@ import asyncio
 from aiohttp import ClientSession
 from discord.ext import commands
 
-from potato_bot.utils import run_process
+from potato_bot.utils import run_process, minutes_to_human_readable
 from potato_bot.checks import is_admin
 
 
@@ -63,24 +63,29 @@ class Misc(commands.Cog):
             if not users:
                 return await ctx.send("No bans recorded yet")
 
+            users = sorted(users, key=lambda u: u.name.lower())
+
+            total_duration = minutes_to_human_readable(sum(u.duration for u in users))
             result = "\n".join(
-                f"{i + 1:>2}. {user.name}: {user.ban_count} bans, {user.duration} minutes"
+                f"{i + 1:>2}. {user.name}: {user.ban_count} bans, {minutes_to_human_readable(user.duration)}"
                 for i, user in enumerate(users)
             )
 
-            title = f"Bans: {sum(u.ban_count for u in users)}\nDuration: {sum(u.duration for u in users)}"
+            title = (
+                f"Bans: **{sum(u.ban_count for u in users)}**\nDuration: **{total_duration}**"
+            )
             return await ctx.send(f"{title}```{result}```")
 
         bans = await self.bot.bans_db.fetch_user_bans(user_name)
         if not bans:
             return await ctx.send("No bans recorded for user")
 
-        total_duration = sum(ban.minutes for ban in bans)
+        total_duration = minutes_to_human_readable(sum(ban.minutes for ban in bans))
         result = "\n".join(
             f"{i + 1:>2}{'.' if ban.expired else '!'} {ban.admin_name}: {ban.title}"
             for i, ban in enumerate(bans)
         )
 
         await ctx.send(
-            f"User has {len(bans)} ban(s) for {total_duration} minutes in total```{result}```"
+            f"User has **{len(bans)}** ban(s) for **{total_duration}** in total```{result}```"
         )
