277934e0d92c5c04d9e73736100a6aa856d5c960
Eugene <fogaprod@gmail.com>
2021-01-04 16:41:13 +0300

    HONK on every line

--- potato_bot/cogs/accents/clown.py
+++ potato_bot/cogs/accents/clown.py
@@ -1,11 +1,19 @@
+import re
 import random
 
+from typing import Optional
+
 from .accent import Accent
+
+
+def honk(m: re.Match) -> Optional[str]:
+    return f"{' HONK' * random.randint(1, 4)}!"
 
 
 # https://github.com/unitystation/unitystation/blob/cf3bfff6563f0b3d47752e19021ab145ae318736/UnityProject/Assets/Resources/ScriptableObjects/Speech/Clown.asset
 class Clown(Accent):
     REPLACEMENTS = {
         r"[a-z]": lambda m: m[0].upper(),
-        Accent.MESSAGE_END: lambda m: f"{' HONK' * random.randint(1, 4)}!",
+        r"(?<!```)\n": lambda m: f"{honk(m)}\n",
+        Accent.MESSAGE_END: honk,
     }
