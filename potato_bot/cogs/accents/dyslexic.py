288dec0549abc67614743d5d597a18ea3f194ac7
Eugene <fogaprod@gmail.com>
2021-01-04 16:31:39 +0300

    Swap words in dyslexic accent

--- potato_bot/cogs/accents/dyslexic.py
+++ potato_bot/cogs/accents/dyslexic.py
@@ -5,5 +5,11 @@ from .accent import Accent
 
 class Dyslexic(Accent):
     REPLACEMENTS = {
-        r"[a-z]{2}": lambda m: m[0] if random.random() < 0.90 else m[0][::-1]
+        # swap words with 5% chance
+        r"\b(\w+?)(\s+)(\w+?)\b": lambda m: m[0]
+        if random.random() < 0.95
+        else f"{m[3]}{m[2]}{m[1]}",
+        # swap letters with 5% chance
+        # NOTE: lower() is used to let the replace function handle case
+        r"[a-z]{2}": lambda m: m[0] if random.random() < 0.95 else m[0][::-1].lower(),
     }
