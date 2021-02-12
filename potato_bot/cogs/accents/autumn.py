05c502cd633f7dc10ed0c53641e686871a26fa79
Eugene <gish.ee18@gmail.com>
2021-02-12 09:56:07 +0300

    First take on Autumn accent

---
+++ potato_bot/cogs/accents/autumn.py
@@ -0,0 +1,34 @@
+import random
+
+from .accent import Accent
+
+
+class Autumn(Accent):
+    WORD_REPLACEMENTS = {
+        r"increases?": (
+            lambda m: f"go br{'r' * random.randint(1, 10)}",
+            None,
+        ),
+        "them": "em",
+        "well": "welp",
+        "oh": (
+            "er",
+            "crap",
+            None,
+        ),
+        "hey": "yo",
+    }
+    REPLACEMENTS = {
+        r"t m": "mm",
+        r"wh": "w",
+        r"oo": "u",
+        r"ng\b": (
+            "n",
+            "n'",
+        ),
+        r"'?ve been": "da",
+        Accent.MESSAGE_END: {
+            " ye daft cunt": 1,
+            None: 50,
+        },
+    }
