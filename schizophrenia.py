7db0dd4bc1307c5cac91e200e52d031dee171d88
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2021-02-19 22:12:33 +0300

    Revert "aaaaah (#4)"
    This reverts commit cd135b97e340a937b079135e929e3afa10ddbf21.

--- schizophrenia.py
+++ schizophrenia.py
@@ -108,3 +108,4 @@ class Schizophrenia(Accent):
         r"\w+": {switch_topic: 2, repeat_word: 3, None: 10},
         f"({'|'.join(re.escape(word) for word in ending)})": generate_neologism(), #if new word matches any from 'ending' massive its replaced by 'generate_neologism()'
     }
+
