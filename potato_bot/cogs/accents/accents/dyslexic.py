1beb52bcbb09d9ec6d8228ea60fa58b1532b547a
Eugene <fogaprod@gmail.com>
2021-02-19 20:43:45 +0300

    Cleaner dyslexic rules

--- potato_bot/cogs/accents/accents/dyslexic.py
+++ potato_bot/cogs/accents/accents/dyslexic.py
@@ -1,17 +1,15 @@
-import random
-
 from .accent import Accent
 
 
 class Dyslexic(Accent):
     REPLACEMENTS = {
         # swap words with 5% chance
-        r"\b(\w+?)(\s+)(\w+?)\b": lambda m: m.original
-        if random.random() < 0.95
-        else f"{m.match[3]}{m.match[2]}{m.match[1]}",
+        r"\b(\w+?)(\s+)(\w+?)\b": {
+            lambda m: f"{m.match[3]}{m.match[2]}{m.match[1]}": 0.05,
+        },
         # swap letters with 5% chance
         # NOTE: lower() is used to let the replace function handle case
-        r"[a-z]{2}": lambda m: m.original
-        if random.random() < 0.95
-        else m.original[::-1].lower(),
+        r"[a-z]{2}": {
+            lambda m: m.original[::-1].lower(): 0.05,
+        },
     }
